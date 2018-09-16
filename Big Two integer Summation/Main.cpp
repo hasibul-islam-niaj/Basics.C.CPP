6a05947243f3ff27d4ef29f9dd9499608d865763
hasibul-islam-niaj <hasibulislamcse@gmail.com>
2018-09-17 00:09:17 +0600

    how big number is possible to enter by you, it can be summation

---
+++ Big Two integer Summation/Main.cpp
@@ -0,0 +1,67 @@
+//
+// Created by hasibul on 8/23/18.
+//
+#include <iostream>
+using namespace std;
+
+class BigIntegerAddition{
+public:
+    string add(string firstNum, string secondNum);
+};
+
+string BigIntegerAddition::add(string firstNum, string secondNum) {
+    int fnLen = firstNum.length();
+    int snLen = secondNum.length();
+
+    string result = "";
+
+    int length;
+
+    if (fnLen > snLen) {
+        for (int i = 0; i < fnLen - snLen; i++){
+            secondNum = "0" + secondNum;
+        }
+        length = fnLen;
+    }
+    else {
+        for (int i=0; i<snLen-fnLen; i++){
+            firstNum = "0" + firstNum;
+        }
+        length = snLen;
+    }
+
+    int a, b;
+    string carry = "0";
+
+    for (int i=length-1; i>=0; i--){
+        a = ((int) firstNum[i])- 48;
+        b = ((int) secondNum[i]) - 48;
+
+        if (carry != "0"){
+            a += ((int) carry[0]) - 48;
+            carry = "0";
+        }
+
+        if (to_string(a+b).length() > 1){
+            carry = to_string(a+b)[0];
+        }
+
+        if (carry == "0")
+            result = to_string(a+b) + result;
+        else
+            result = to_string(a+b)[1] + result;
+    }
+
+    if (carry != "0"){
+        result = carry + result;
+    }
+
+    cout<<result<<endl;
+
+    return result;
+}
+
+int main(){
+    BigIntegerAddition math;
+    math.add("1", "999");
+}
